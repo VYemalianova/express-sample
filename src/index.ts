41e0574ab3fc145de711c64eff32d34e75702e85
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2025-07-17 18:59:31 +0300

    init express + ts + nodemon

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
